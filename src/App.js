53295bd6e06377f4bf5d48b4670f4fc5aecade03
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-02-20 16:07:07 +0530

    My First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import ButtonAppBar from './components/navbar';
+import Grid from './components/grid';
+import Footer from './components/footer';
+import { Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    subtitle1: {
+      fontSize: 12,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      fontStyle: 'italic',
+    },
+  },
+});
 
 function App() {
+  const mystyle = {
+    marginTop:"20px",
+    padding: "10px",
+    textAlign:"center"
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{textAlign: "center"}}>
+      <ButtonAppBar/>
+      <div style={{marginTop: "100px"}}>
+        <ThemeProvider theme={theme}>
+          <Typography variant="h2">Lorem Ipsum</Typography>
+          <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam rhoncus velit nisl, 
+            eu porttitor sem dictum mattis. Donec dapibus faucibus placerat. Ut feugiat nunc ac egestas posuere. <br/>
+            Ut dignissim, quam porta pulvinar commodo, enim leo placerat sem, nec ultricies quam purus ut erat. 
+            Ut commodo semper eros id mattis. Morbi varius vulputate nisi. <br/>Pellentesque eget purus a elit volutpat 
+            feugiat ut sit amet nibh.</Typography>
+        </ThemeProvider>
+      </div>
+      <div style={mystyle}>
+        <Grid/>
+      </div>
+      
+      <Footer/>
     </div>
+    
   );
 }
-
 export default App;
